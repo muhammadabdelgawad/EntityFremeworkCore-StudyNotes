62e22d3f7f4f1b7aa2c4ff78d640db6c5b1d650a
Muhammad Abd-Elgawad <muhammad@gamil.com>
2025-12-01 22:06:01 +0200

    Introduce EF Core and add custom SQL migration
    The `Program.cs` file was updated to remove the default "Hello, World!" message and `Console.ReadLine()` statement. EF Core was added, and an `AppDbContext` instance was created. Code for adding an `Employee` entity was commented out for potential future use.
    
    A new migration, `20251201200357_TestToAddaAnEmptyMigrationAndWriteInItAQuery.cs`, was added. This migration includes custom SQL to insert and delete an employee record in the `Employees` table.
    
    The corresponding designer file, `20251201200357_TestToAddaAnEmptyMigrationAndWriteInItAQuery.Designer.cs`, was auto-generated to reflect the updated database schema, including the `Employees` table with `Id` and `Name` columns.

---
+++ EFCore/Migrations/20251201200357_TestToAddaAnEmptyMigrationAndWriteInItAQuery.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestToAddaAnEmptyMigrationAndWriteInItAQuery : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Employees VALUES ('Mu ForTest')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Employees WHERE Name = 'Mu ForTest'");
+
+        }
+    }
+}
--- EFCore/Program.cs
+++ EFCore/Program.cs
@@ -1,2 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿using EFCore;
+
+var _dbContext = new AppDbContext();
+
+//var employee01 = new Employee
+//{
+//    Name = "Muhammad"
+//};
+
+//_dbContext.Employees.Add(employee01);
+//_dbContext.SaveChanges();
+
+
+
+
+
+
+
+
+
+
+
