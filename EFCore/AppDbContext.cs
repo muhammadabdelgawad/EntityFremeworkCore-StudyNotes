2fdae12eaca24e83eeceda4261005a94321502f1
Muhammad Abd-Elgawad <muhammad@gamil.com>
2025-12-01 20:26:27 +0200

    Add EF Core setup with Employee entity and migration
    Introduced Entity Framework Core setup for a SQL Server database:
    - Added `Microsoft.EntityFrameworkCore.Design` package to `EFCore.csproj`.
    - Created `AppDbContext` to manage database interactions.
    - Defined `Employee` entity with `Id` and `Name` properties.
    - Added migration to create `Employees` table in the database.
    - Generated migration designer and updated model snapshot.

---
+++ EFCore/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace EFCore
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer(@"Server = MUHAMMAD\SQLEXPRESS;Database=EFCore;Trusted_Connection=True;TrustServerCertificate=True;");
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
---
+++ EFCore/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace EFCore
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
